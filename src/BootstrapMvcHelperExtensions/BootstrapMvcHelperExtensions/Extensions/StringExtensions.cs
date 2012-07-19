704111633632fd9e6be6a9b4d76e74f9e2365a41
devdaves <info@devdave.com>
2012-07-18 22:16:24 -0700

    initial project creation, inlcudes initial copy of bootstrap extensions, in need of refactoring

---
+++ src/BootstrapMvcHelperExtensions/BootstrapMvcHelperExtensions/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace BootstrapMvcHelperExtensions.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// String Extension methods
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Converts a string to an MVC HTML string
+        /// </summary>
+        /// <param name="stringValue">The string value.</param>
+        /// <returns>An MvcHtmlString</returns>
+        public static MvcHtmlString ToMvcHtmlString(this string stringValue)
+        {
+            return new MvcHtmlString(stringValue);
+        }
+    }
+}
---
+++ src/BootstrapMvcHelperExtensions/BootstrapMvcHelperExtensions/Extensions/TagBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿namespace BootstrapMvcHelperExtensions.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Tag Builder Extension methods
+    /// </summary>
+    public static class TagBuilderExtensions
+    {
+        /// <summary>
+        /// Convert tagbuilder to an MVC HTML string.
+        /// </summary>
+        /// <param name="tagBuilder">The tag builder.</param>
+        /// <returns>An MvcHtmlString</returns>
+        public static MvcHtmlString ToMvcHtmlString(this TagBuilder tagBuilder)
+        {
+            return tagBuilder.ToString().ToMvcHtmlString();
+        }
+
+        /// <summary>
+        /// Convert tagbuilder to an MVC HTML string.
+        /// </summary>
+        /// <param name="tagBuilder">The tag builder.</param>
+        /// <param name="tagRenderMode">The tag render mode.</param>
+        /// <returns>An MvcHtmlString</returns>
+        public static MvcHtmlString ToMvcHtmlString(this TagBuilder tagBuilder, TagRenderMode tagRenderMode)
+        {
+            return tagBuilder.ToString(tagRenderMode).ToMvcHtmlString();
+        }
+    }
+}
